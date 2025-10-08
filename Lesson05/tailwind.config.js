d547d76d4076edfefeedfc3fa130e67f382b8360
sjtroxel <sjtroxel@protonmail.com>
2025-10-08 17:42:19 -0500

    Lesson 5 finished, intro to tailwind tutorial is complete

--- Lesson05/tailwind.config.js
+++ Lesson05/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*html", "./build/js/*.js"],
+  content: ["./build/**/*.{html,js}"], // don't do this with just {html}
   theme: {
     extend: {
+      colors: {
+        papayawhip: {
+          light: '#fef4e4',
+          DEFAULT: '#ffefd5',
+          dark: '#fee5bc',
+        }
+      },
       screens: {
         'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
-        'tallscreen': { 'raw': '(min-aspect-ratio: 13/20)' },
+        'tallscreen': { 'raw': '(max-aspect-ratio: 13/20)' },
       },
       keyframes: {
         'open-menu': {
